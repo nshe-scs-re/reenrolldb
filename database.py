5e090db8c82da5ea0d4000331b009eaa5098f67f
Yi-Hsuan Chuang <yihsuanc@unr.edu>
2023-09-29 14:25:58 -0700

    updated files

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from .config import settings
+from config import settings
 
 
-#sqlite3
+# sqlite3
 CONNECTION_STRING = settings.connection_string
 
-#postgresql
-#CONNECTION_STRING = "postgresql://{0}:{1}@{2}/{3}".format(settings.username,
+# postgresql
+# CONNECTION_STRING = "postgresql://{0}:{1}@{2}/{3}".format(settings.username,
 #                                                          settings.password,
 #                                                          settings.hostname,
 #                                                          settings.database)
